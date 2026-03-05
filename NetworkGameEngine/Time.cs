0c322bfedf56b6eb3f6f488c05819e79700304af
RealmCraftsman <vyacheslav.bysel@gmail.com>
2026-03-05 21:11:10 +0100

    - Added DeltaTime to Time

--- NetworkGameEngine/Time.cs
+++ NetworkGameEngine/Time.cs
@@ -4,22 +4,29 @@
     {
         private int m_tick = 0;
         private long m_startTickTime;
+        private long m_lastTickTime;
+        private int m_deltaTime;
         public readonly long FixedDeltaTimeMillis = 100;
         public readonly float FixedDeltaTimeSeconds = 0.1f;
 
         public int Tick => m_tick;
+        public int DeltaTime => m_deltaTime;
+        public float DeltaTimeSeconds => m_deltaTime / 1000f;
 
         public Time(int tickInterval)
         {
             FixedDeltaTimeMillis = tickInterval;
             FixedDeltaTimeSeconds = tickInterval / 1000f;
             m_startTickTime = Milliseconds;
+            m_lastTickTime = Milliseconds;
         }
 
-        public void NextTick()
+        internal void NextTick()
         {
             m_tick++;
             m_startTickTime = Milliseconds;
+            m_deltaTime = (int)(m_startTickTime - m_lastTickTime);
+            m_lastTickTime = m_startTickTime;
         }
 
         internal int CalculateSleepTime()
